215761f51a78193d516be9379ed77102aaeefeb3
Eva Lee <elee@auction.com>
2016-03-18 08:40:48 -0700

    fix type and tablename

--- src/main/java/com/tenx/ms/retail/product/domain/ProductEntity.java
+++ src/main/java/com/tenx/ms/retail/product/domain/ProductEntity.java
@@ -28,7 +28,7 @@ public class ProductEntity {
     @NotNull
     @ManyToMany
     @JoinTable(
-            name = "stock",
+            name = "stocks",
             joinColumns = {@JoinColumn(name = "product_id", referencedColumnName = "product_id")},
             inverseJoinColumns = {@JoinColumn(name = "store_id", referencedColumnName = "store_id")})
     private Set<StoreEntity> stores = new HashSet<>();
